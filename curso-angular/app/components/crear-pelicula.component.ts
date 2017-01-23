9f6f8168a4f541d59ed271c279d24074f07bfe38
jzena <jose.zena.zamora@gmail.com>
2017-01-23 15:45:51 -0500

    Proyecto adumy

---
+++ curso-angular/app/components/crear-pelicula.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from "angular2/core";
+import { Pelicula } from "../model/pelicula";
+import { Router, RouteParams } from "angular2/router";
+import { PeliculasService } from "../services/peliculas.service";
+
+@Component({
+    templateUrl: "app/view/crear-pelicula.html",
+    providers: [PeliculasService]
+})
+
+export class CrearPeliculaComponent implements OnInit {
+    public TituloPelicula = "";
+    public nuevaPelicula: Pelicula;
+
+    constructor(private _peliculasService: PeliculasService,
+        private _router: Router,
+        private _routeParams: RouteParams) {
+
+    }
+
+    onSubmit() {
+        this._peliculasService.insertPelicula(this.nuevaPelicula);
+        //console.log(pelicula);
+        this._router.navigate(["Peliculas"]);
+    }
+
+    ngOnInit(): any {
+        this.TituloPelicula = this._routeParams.get("titulo");
+        this.nuevaPelicula = new Pelicula(
+            0,
+            this._routeParams.get("titulo"),
+            this._routeParams.get("director"),
+            parseInt(this._routeParams.get("anio"))
+        );
+        //CrearPeliculaBasadaEnOtra
+    }
+
+}
---
+++ curso-angular/app/services/mock.peliculas.ts
@@ -0,0 +1,10 @@
+import { Pelicula } from '../model/pelicula';
+
+export const PELICULAS: Pelicula[] = [
+    new Pelicula(1, "Batman vs superman", "Zack Snider", 2016),
+    new Pelicula(2, "La verdad duele", "Will smith", 2016),
+    new Pelicula(3, "El señor de los anillos", "Desconocido", 2014),
+    new Pelicula(4, "Una historia real", "El de supersalidos", 2015),
+    new Pelicula(5, "Don Jon", "Josep Gordon Levit", 2014),
+    new Pelicula(6, "Dos tontos muy tontos", "Desconocido", 2014)
+];

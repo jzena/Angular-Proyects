ec06754493abd8218457914ce11a553a29507b6b
jzena <jose.zena.zamora@gmail.com>
2017-01-24 15:23:14 -0500

    proyecto angular

---
+++ proyecto-angular/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from 'angular2/core';
+import { RestaurantesListComponent } from './components/restaurantes-list.component';
+import { RestaurantesDetailComponent } from './components/restaurantes-detail.component';
+
+import { ROUTER_DIRECTIVES, RouteConfig, Router } from 'angular2/router';
+
+@Component({
+    selector: "mi-app",
+    templateUrl: "app/view/home.html",
+    directives: [RestaurantesListComponent, ROUTER_DIRECTIVES]
+})
+
+@RouteConfig([
+    { path: '/', name: "Home", component: RestaurantesListComponent, useAsDefault: true },
+    { path: '/restaurante/:id', name: "Restaurante", component: RestaurantesDetailComponent }
+])
+
+export class AppComponent {
+    public titulo: string = "Restaurantes";
+}
